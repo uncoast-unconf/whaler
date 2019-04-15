d7ba142cd79938e176187bc76801c7a3cc3206b0
Amanda Gadrow <amanda.gadrow@rstudio.com>
2019-04-15 16:35:02 -0500

    Add function to pull system dependencies for R packages

--- R/packages.R
+++ R/packages.R
@@ -3,6 +3,9 @@
 
 library(packrat)
 library(dplyr)
+library(httr)
+library(jsonlite)
+library(purrr)
 
 source("utils.R")
 
@@ -23,4 +26,25 @@ get_packages_list <- function(project_dir = getwd()) {
   r_packages <- df[-1, 2:3]
   packages_list <- list("r_version" = r_version, "r_packages" = r_packages)
   packages_list
+}
+
+#
+# WARNING: This API is currently down (15-Apr 2019) so this function does not work
+#
+# Use https://sysreqs.r-hub.io/ to get system requirements for the vector of R packages
+# Pass in a dataframe of packages and versions, and the operating system for which you want the list of dependencies
+# packages_df can be the second element of the list output of get_packages_list(), e.g.,
+#
+# package_list <- get_packages_list()
+# packages_df <- package_list$r_packages
+#
+# The function takes the entire dataframe and parses out packages_df$Package explicitly,
+# in case the packages_df$Version becomes useful later.
+#
+get_pkg_sys_deps <- function(packages_df, os) {
+  endpoint <- "https://sysreqs.r-hub.org/pkg/"
+  calls <- paste0(endpoint, packages_df$Package, "/", os)
+  sysdeps <- map(calls, httr::GET)
+  # TODO: when API is back up, may need to clean up the responses before outputting from this function
+  sysdeps
 }
