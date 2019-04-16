a09c87f2959e23f0fd58109d7c4401b818b3d8d5
Sam Helmich <HelmichSamR@JohnDeere.com>
2019-04-16 13:47:54 -0500

    added documentation and returns file string

--- R/create_current_dockerfile.R
+++ R/create_current_dockerfile.R
@@ -1,10 +1,19 @@
-create_current_dockerfile <- function(){
+#' Creates a dockerfile that mirrors the current environment
+#'
+#' @param `String` output dockerfile location and name, defaults to "Dockerfile"
+#'
+#' @return
+#' @export `String` outputs dockerfile location and anme
+#' #'
+#' @examples
+#' create_current_dockerfile()
+create_current_dockerfile <- function(file = "Dockerfile"){
   # Gets the current R version
   ver <- strsplit(R.version.string, split=" ")[[1]][3]
 
   dockerfile() %>%
     set_from(paste("rocker/r-ver:", ver)) %>%
     add_environ_packages() %>%
-    write_dockerfile(overwrite = TRUE)
+    write_dockerfile(file = file, overwrite = TRUE)
 
 }
